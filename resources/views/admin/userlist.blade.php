dd27f75a159252ae34172b80ba91323c2d1e56e5
Jerryy25 <jherryyhusuf25@gmail.com>
2020-07-29 00:12:40 +0700

    Project-Laravel-master

---
+++ resources/views/admin/userlist.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data User</title>
+</head>
+<body>
+    <div class="container">
+        <table>
+            <tr>
+                <td>No</td>
+                <td>Nama user</td>
+                <td>Email</td>
+                <td>Status</td>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>({$user1})</td>
+                <td>aliffio1615@gmail.com</td>
+                <td>Aktif</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>({$user2})</td>
+                <td>deva.abel@gmail.com</td>
+                <td>Aktif</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>({$user3})</td>
+                <td>akmal.sukma@gmail.com</td>
+                <td>Aktif</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>({$user4})</td>
+                <td>akmal.sukma@gmail.com</td>
+                <td>Aktif</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
---
+++ resources/views/myblog/home.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Blog</title>
+
+</head>
+<body>
+
+  <style>
+      .jumbotron{
+          background: url(img/marble.png) center center;
+      }
+      .tulisan{
+        font-size: 20px;
+        font-family: Roboto;
+      }
+      .display-5{
+        color: black;
+        font-size: 20px;
+        font-family: Roboto;
+      }
+      .navbar-nav{
+        font-family: Roboto;
+        font-weight: bold;
+        
+      }
+      
+  </style>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary">
+    <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+<div class="navbar-nav text-white" style="font-size: 20px;"> 
+  <a class="nav-item nav-link" href="home">HOME <span class="sr-only">(current)</span></a>
+    <a class="nav-item nav-link" href="#profil">PROFIL</a>
+      <a class="nav-item nav-link" href="experience">EXPERIENCE</a>
+        <a class="nav-item nav-link" href="portofolio">PORTOFOLIO</a>
+      </div>
+    </div>
+</nav>
+
+  <div class="jumbotron">
+    <center>
+      <img src="img/avatar.png" width="200px" class="rounded-circle">
+      <h1></h1>
+        <h1 class="display-5" >Jerry Yusuf S</h1>
+      <p class="display-5">
+          (Siswa / Student)
+      </p>
+  <p class="display-5">SMK Taruna Bhakti</p>
+    </center>
+  </div>
+
+
+ <div class="container">
+ <div class="row">
+    <div class="col text-center">
+      <h1 id="profil" style="font-family: Roboto">PROFILE</h1><br>
+    </div>
+  </div>
+    <p class="tulisan">
+    Nama saya Jerry Yusuf S, atau biasa dipanggil Jerry. Saya lahir di Depok, 05 November 2002 dan saat ini saya menginjak usia 17 tahun. 
+    Saat ini, saya bersekolah / menempuh ilmu di SMK Taruna Bhakti Depok, dan saya sekarang berada di tingkat atau kelas 12 untuk jurusan RPL (Rekayasa Perangkat Lunak). Dan berikut adalah data diri lengkap saya.
+    <br><br>
+
+<!-- Biodata -->
+        Nama : Jerry Yusuf S
+        <br>
+        Kelas : XII RPL 1
+        <br>
+        Sekolah : SMK Taruna Bhakti
+        <br>
+        Tempat, Tanggal Lahir : Depok, 05 November 2002
+        <br>
+        Alamat : Kp. Sindangkarsa Jalan Jian RT 05 RW 09 No. 51, Kel. Sukamaju Baru, Kec.Tapos, Kota Depok
+        <br>
+        Agama : Khatolik
+        <br>
+        Jenis Kelamin : laki-laki
+        <br>
+        Email : jherryyhusuf25@gmail.com
+    <br><br><br>
+    </p>
+  </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
